ea0977c9d0ff0deab23e879e5fd981b7a10ddf0c
Camilo Villegas <cam.aristi12@gmail.com>
2025-05-15 23:12:37 -0500

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,13 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
-    <div className="container footer">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <p className="col-md-4 mb-0 text-body-secondary">© 2025 Company, Primax</p>
-        <ul className="nav col-md-4 justify-content-end">
-          <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
-          <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
-          <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
-          <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Contáctanos</a></li>
-          <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Acerca de</a></li>
-        </ul>
+      <footer className="container-fluid">
+          <div className="container my-auto">
+              <div className="copyright text-center my-auto">
+                  <span>© 2025 Company, Primax</span>
+              </div>
+          </div>
       </footer>
-    </div>
     </>
   )
 }
